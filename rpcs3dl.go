2c6d6ccb1021112f6f2d05aad520cb769b40f524
Zom Ponks <zom-ponks@outlook.com>
2020-04-21 14:05:00 +0300

    Some slight code reorg

--- rpcs3dl.go
+++ rpcs3dl.go
@@ -16,45 +16,6 @@ import (
 	"strings"
 )
 
-/* constants */
-
-const urlPattern string = "https://a0.ww.np.dl.playstation.net/tpl/np/%s/%s-ver.xml"
-const gamesYAML = "games.yml"
-
-/* structures */
-
-/* this is the sony titlepatch format */
-
-// Paramsfo contains the title of the game
-type Paramsfo struct {
-	TITLE string `xml:"TITLE"`
-}
-
-// Package contains the actual patch file along with metadata (size, sha1)
-type Package struct {
-	Size     string   `xml:"size,attr"`
-	SHA1     string   `xml:"sha1sum,attr"`
-	Paramsfo Paramsfo `xml:"paramsfo"`
-	URL      string   `xml:"url,attr"`
-	Version  string   `xml:"version,attr"`
-}
-
-// Tag wraps packages
-type Tag struct {
-	// we might have multiple patches in a title
-	Package []Package `xml:"package"`
-	Name    string    `xml:"name,attr"`
-	Popup   string    `xml:"popup,attr"`
-	Signoff string    `xml:"signoff,attr"`
-}
-
-// TitlePatch contains all of the patch data for a given title
-type TitlePatch struct {
-	Tag     Tag    `xml:"tag"`
-	Status  string `xml:"status,attr"`
-	Titleid string `xml:"titleid,attr"`
-}
-
 // just a generic helper
 
 func isError(err error) bool {
